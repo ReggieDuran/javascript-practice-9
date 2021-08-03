76d691b50040c56484d6ebda291768243824d029
Reggie <reggieduran15@gmail.com>
2021-08-03 10:01:05 +0800

    Event delegation and trigger event programmatically

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 // button.onclick = function() {
 
@@ -36,5 +36,33 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
-    console.log(event)
+    console.log(event);
+});
+
+const div = document.querySelector('div');
+
+div.addEventListener('mouseenter', event => {
+    console.log('CLICKED DIV');
+    console.log(event);
+});
+
+button.addEventListener('click', event => {
+    console.log('CLICKED BUTTON');
+    console.log(event);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//     listItem.addEventListener('click', event => {
+//         event.target.classList.toggle('highlight');
+//     });
+// });
+
+list.addEventListener('click',  event => {
+    // console.log(event.currentTarget);
+    // event.target.classList.toggle('highlight');
+    event.target.closest('li').classList.toggle('highlight');
+    button.click();
 });
